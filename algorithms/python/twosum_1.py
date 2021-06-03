49ce6748c9e536978370cf0f0a099829bdf7de67
takuan517 <ishitaku-2000517@keio.jp>
2021-06-03 12:08:50 +0900

    twosum

---
+++ algorithms/python/twosum_1.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, n1 in enumerate(nums):
+            if target - n1 in nums:
+                for j , n2 in enumerate(nums):
+                    if i != j and n1 + n2 == target:
+                        return [i, j]
+
