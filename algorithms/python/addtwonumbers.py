d8eff085ac45a66d113725e79df2f2bd2c0d080c
takuan <50577904+takuan517@users.noreply.github.com>
2021-06-06 00:23:14 +0900

    Update addtwonumbers.py

--- algorithms/python/addtwonumbers.py
+++ algorithms/python/addtwonumbers.py
@@ -1,22 +1,34 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    def reverse(self, x: int) -> int:
-            hoge = str(x)
-            print(hoge)
-            if hoge[0] == '-':
-                a = hoge[0]
-                b = hoge[1:]
-                c = b[::-1]
-                d = a + c
-                result = int(d)
-                if result >= 2147483647 or result <= -2147483648:
-                    return 0
-                else:
-                    return result
-            else:
-                hage = hoge[::-1]
-                print(hage)
-                result = int(hage)
-                if result >= 2147483647 or result <= -2147483648:
-                    return 0
-                else:
-                    return result
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        result = ListNode(0)
+        current = result
+        list1 = []
+        list2 = []
+        hage = 0
+        while(l1.next != None):
+            list1.append(l1.val)
+            l1 = l1.next
+        list1.append(l1.val)
+        while(l2.next != None):
+            list2.append(l2.val)
+            l2 = l2.next
+        list2.append(l2.val)
+        hoge = list(map(sum, zip(list1, list2)))
+        if len(list1) > len(list2):
+            hoge.extend(list1[len(hoge):])
+        elif len(list2) > len(list1):
+            hoge.extend(list2[len(hoge):])
+        print(hoge)
+        for i in hoge:
+            unko = i + hage
+            current.next = ListNode(unko%10)
+            hage = int(unko / 10)
+            current = current.next
+        if hage != 0:
+            current.next = ListNode(hage)
+        return result.next
